3c9c0af25dce052c3045545c9a2425878014e4b2
Stealwin <steal1991@yandex.ru>
2021-12-12 22:10:13 +0300

    add description of card

--- src/components/item-basket/index.js
+++ src/components/item-basket/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import propTypes from 'prop-types';
 import numberFormat from "../../utils/number-format";
 import './styles.css';
+import {Link} from "react-router-dom";
 
 function ItemBasket({item}) {
   return (
     <div className='ItemBasket'>
+      <Link to="/descr" state={{item}} className="ItemBasket__left" >
       <div className='ItemBasket__number'>{item._key}</div>
       <div className='ItemBasket__title'>{item.title}</div>
+      </Link>
       <div className='ItemBasket__right'>
         <span className="ItemBasket__cell">{numberFormat(item.price || 0)} ₽</span>
         <span className="ItemBasket__cell">{numberFormat(item.amount || 0)} шт</span>
